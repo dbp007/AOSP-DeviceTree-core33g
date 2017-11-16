8433dc42ab0f9f048e2829ffea4aebfe30340c42
LoneFlux <theloneflux@gmail.com>
2017-11-16 07:31:13 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,28 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit from scx30g2 common configs
--include device/samsung/scx30g2-common/BoardConfigCommon.mk
+# Inherit from scx30g common configs
+-include device/samsung/scx30g-common/BoardConfigCommon.mk
 
 # Inherit from the proprietary version
--include vendor/samsung/grandprimeve3g/BoardConfigVendor.mk
+-include vendor/samsung/core33g/BoardConfigVendor.mk
 
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := SC7730SE
+TARGET_BOOTLOADER_BOARD_NAME := sc7730s
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 5872025600
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 6094323712
 BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 
-# Wifi
+# WiFi
 BOARD_WLAN_DEVICE := bcmdhd
 BOARD_WLAN_DEVICE_REV := bcm4343
 WPA_SUPPLICANT_VERSION := VER_0_8_X
@@ -49,109 +48,16 @@ WIFI_DRIVER_NVRAM_PATH := "/system/etc/wifi/nvram_net.txt"
 WIFI_BAND := 802_11_ABG
 BOARD_HAVE_SAMSUNG_WIFI := true
 
-# Camera
-#zsl capture
-TARGET_BOARD_CAMERA_CAPTURE_MODE := false
-
-#back camera rotation capture
-TARGET_BOARD_BACK_CAMERA_ROTATION := false
-
-#front camera rotation capture
-TARGET_BOARD_FRONT_CAMERA_ROTATION := false
-
-#rotation capture
-TARGET_BOARD_CAMERA_ROTATION_CAPTURE := false
-
-#support hal1.0,hal3.2
-TARGET_BOARD_CAMERA_HAL_VERSION := 1.0
-
-# camera sensor type
-CAMERA_SENSOR_TYPE_BACK := "s5k4h5yc_mipi"
-CAMERA_SENSOR_TYPE_FRONT := "s5k5e3yx_mipi"
-
-# select camera 2M,3M,5M,8M
-CAMERA_SUPPORT_SIZE := 8M
-FRONT_CAMERA_SUPPORT_SIZE := 5M
-TARGET_BOARD_NO_FRONT_SENSOR := false
-TARGET_BOARD_CAMERA_FLASH_CTRL := false
-
-#read sensor otp to isp
-TARGET_BOARD_CAMERA_READOTP_TO_ISP := true
-
-# use sprd auto lens
-TARGET_BOARD_CAMERA_SPRD_AUTOLENS := false
-
-#otp version, v0(OTP on Grandprime, Z3) v1(OTP on J1MINI) v2(Without OTP on TabG)
-TARGET_BOARD_CAMERA_OTP_VERSION := 0
-
-#read otp method 1:from kernel 0:from user
-TARGET_BOARD_CAMERA_READOTP_METHOD := 1
-
-#face detect
-TARGET_BOARD_CAMERA_FACE_DETECT := true
-TARGET_BOARD_CAMERA_FD_LIB := omron
-
-#sensor interface
-TARGET_BOARD_BACK_CAMERA_INTERFACE := mipi
-TARGET_BOARD_FRONT_CAMERA_INTERFACE := mipi
-
-#select camera zsl cap mode
-TARGET_BOARD_CAMERA_CAPTURE_MODE := true
-
-#select camera zsl force cap mode
-TARGET_BOARD_CAMERA_FORCE_ZSL_MODE := true
-
-#rotation capture
-TARGET_BOARD_CAMERA_ROTATION_CAPTURE := false
-
-#select camera not support autofocus
-TARGET_BOARD_CAMERA_NO_AUTOFOCUS_DEV := false
-
-#uv denoise enable
-TARGET_BOARD_CAMERA_CAPTURE_DENOISE := false
-
-#y denoise enable
-TARGET_BOARD_CAMERA_Y_DENOISE := true
-
-#select continuous auto focus
-TARGET_BOARD_CAMERA_CAF := true
-
-#select ACuteLogic awb algorithm
-TARGET_BOARD_USE_ALC_AWB := true
-
-#pre_allocate capture memory
-TARGET_BOARD_CAMERA_PRE_ALLOC_CAPTURE_MEM := true
-
-#sc8830g isp ver 0;sc9630 isp ver 1;tshark2 isp version 2
-TARGET_BOARD_CAMERA_ISP_SOFTWARE_VERSION := 2
-
-#support auto anti-flicker
-TARGET_BOARD_CAMERA_ANTI_FLICKER := true
-
-#multi cap memory mode
-TARGET_BOARD_MULTI_CAP_MEM := true
-
-#select mipi d-phy mode(none, phya, phyb, phyab)
-TARGET_BOARD_FRONT_CAMERA_MIPI := phyc
-TARGET_BOARD_BACK_CAMERA_MIPI := phyab
-
-#select ccir pclk src(source0, source1)
-TARGET_BOARD_FRONT_CAMERA_CCIR_PCLK := source0
-TARGET_BOARD_BACK_CAMERA_CCIR_PCLK := source0
-
-#hdr effect enable
-TARGET_BOARD_CAMERA_HDR_CAPTURE := true
-
 # Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/grandprimeve3g/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/core33g/bluetooth
 
 # Kernel
-TARGET_KERNEL_CONFIG := cyanogen_grandprimeve3g_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/sprd
+TARGET_KERNEL_CONFIG := android_core33g_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/core33g
 
 # Resolution
-TARGET_SCREEN_HEIGHT := 960
-TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := SM-G531H,SM-G531BT,grandprimeve3g,grandprimeve3gdtv,grandprimeve3gub,grandprimeve3gxx,grandprimeve3gdtvvj
+TARGET_OTA_ASSERT_DEVICE := SM-G360H,SM-G360HU,core33g,core33gdd,core33gdx
